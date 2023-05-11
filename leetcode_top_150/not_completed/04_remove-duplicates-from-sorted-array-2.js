2a0a5079bc8f20295ad70c281efea2f7c151752d
Adrian333Dev <adrian333dev@gmail.com>
2023-05-11 17:16:24 +0400

    Remove Duplicates from Sorted Array II completed

--- leetcode_top_150/not_completed/04_remove-duplicates-from-sorted-array-2.js
+++ leetcode_top_150/not_completed/04_remove-duplicates-from-sorted-array-2.js
@@ -2,23 +2,45 @@
  * @param {number[]} nums
  * @return {number}
  */
-let removeDuplicates = (nums) => { // ❌❌❌
-	if (nums.length < 2) return nums;
-	console.log(nums.toString());
-	let unique = 1;
-	let currNum = null;
-	let currNumCount = 1;
-	for (let i = 0; i < nums.length; i++) {
+let removeDuplicates = (nums) => {
+	if (nums.length <= 2) return nums.length;
+	let currNum = nums[0];
+	let count = 1;
+	let total = 1;
+	for (let i = 1; i < nums.length; i++) {
 		const n = nums[i];
-		if (n == currNum) {
-			currNumCount++;
-			if (currNumCount > 2) nums[i] = '#';
+		if (n === currNum) {
+			if (count >= 2) nums[i] = '#';
+			else {
+				count++;
+				total++;
+			}
 		} else {
 			currNum = n;
-			currNumCount = 1;
+			count = 1;
+			total++;
 		}
 	}
-	return unique;
+	if (total === nums.length) return total;
+	let hashIdx = nums.indexOf('#');
+	if (!hashIdx) return total;
+	let chaseLen = 1;
+	for (let i = hashIdx; i < nums.length; i++) {
+		let n = nums[i];
+		if (n === '#') chaseLen++;
+		else {
+			nums[hashIdx] = n;
+			nums[i] = '#';
+			hashIdx++;
+		}
+	}
+	return total;
 };
 
-console.log(removeDuplicates([0, 0, 1, 1, 1, 2, 2, 3, 4, 5, 5]));
+console.log(
+	removeDuplicates([0, 1, 1, 1, 1, 1, 1, 1, 2, 3, 3, 3, 4, 4, 4, 4, 4])
+);
+console.log(removeDuplicates([1, 1, 1, 2, 2, 3]));
+console.log(removeDuplicates([0, 0, 1, 1, 1, 1, 2, 3, 3]));
+console.log(removeDuplicates([1, 1]));
+console.log(removeDuplicates([1, 1, 2]));

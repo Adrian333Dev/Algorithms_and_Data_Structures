8c9ccf36f58635f5bd52488798448d0496c439a3
Adrian333Dev <adrian333dev@gmail.com>
2023-05-18 03:12:51 +0400

    Quick sort added

--- implementations/sorting/quick.js
+++ implementations/sorting/quick.js
@@ -1,18 +1,18 @@
 const quickSort = (arr, start = 0, end = arr.length - 1) => {
 	if (end - start + 1 <= 1) return arr;
-
 	const pivot = arr[end];
-  let swapIdx = start;
-  
-  for (let i = start; i < end; i++) {
-    if (arr[i] < pivot) {
-      swap(arr, i, swapIdx);
-      swapIdx++;
-    } 
-  }
-  swap(arr, swapIdx, end);
+	let swapIdx = start;
 
-  return arr;
+	for (let i = start; i < end; i++) {
+		if (arr[i] < pivot) {
+			swap(arr, i, swapIdx);
+			swapIdx++;
+		}
+	}
+	swap(arr, swapIdx, end);
+
+	quickSort(arr, start, swapIdx - 1);
+	quickSort(arr, swapIdx + 1, end);
 };
 
 const swap = (arr, idx1, idx2) => {
@@ -22,5 +22,5 @@ const swap = (arr, idx1, idx2) => {
 // test cases
 
 const arr1 = [6, 2, 4, 1, 3];
-
-console.log(quickSort(arr1)); // [1, 2, 3, 4, 6]
+quickSort(arr1);
+console.log(arr1); // [1, 2, 3, 4, 6]

57a2244e2aaf552221fe4f7284ccb937321fac47
Adrian333Dev <adrian333dev@gmail.com>
2023-05-24 15:54:34 +0400

    Min Stack completed

---
+++ neetcode_practice_150/04-stack/02_min-stack.js
@@ -0,0 +1,59 @@
+class MinStack {
+	constructor() {
+		this.stack = [];
+		this.minStack = [];
+	}
+
+	push(x) {
+		this.stack.push(x);
+		if (!this.minStack.length) this.minStack.push(x);
+		else if (x <= this.getMin()) this.minStack.push(x);
+	}
+
+	pop() {
+		const last = this.stack.pop();
+		if (last <= this.getMin()) this.minStack.pop();
+		return last;
+	}
+
+	top() {
+		return this.stack[this.stack.length - 1];
+	}
+
+	getMin() {
+		return this.minStack[this.minStack.length - 1];
+	}
+
+	print() {
+		console.log('stack:', this.stack.join(', '));
+		console.log('minStack:', this.minStack.join(', '));
+	}
+}
+
+// test MinStack
+
+// ["MinStack","push","push","push","getMin","pop","top","getMin"]
+// [[],[-2],[0],[-3],[],[],[],[]]
+
+const minStack = new MinStack();
+// minStack.push(-2);
+// minStack.push(0);
+// minStack.push(-3);
+// minStack.print(); // stack: -2, 0, -3; minStack: -2, -3
+// console.log(minStack.getMin()); // -3
+// console.log(minStack.pop()); // -3
+// console.log(minStack.top()); // 0
+// console.log(minStack.getMin()); // -2
+// minStack.print(); // stack: -2, 0; minStack: -2
+
+// ["MinStack","push","push","push","getMin","pop","getMin"]
+// [[],[0],[1],[0],[],[],[]]
+
+minStack.push(0);
+minStack.push(1);
+minStack.push(0);
+minStack.print(); // stack: 0, 1, 0; minStack: 0, 0
+console.log(minStack.getMin()); // 0
+console.log(minStack.pop()); // 0
+console.log(minStack.getMin()); // 0
+minStack.print(); // stack: 0, 1; minStack: 0

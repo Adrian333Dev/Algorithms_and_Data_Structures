cdbef0e8d295c77b8d365e2319f82a4aff9b6506
Adrian333Dev <adrian333dev@gmail.com>
2023-05-28 04:22:33 +0400

    Merge k sorted arrays updated

--- implementations/sorting/challenges/merge-k-sorted-lists.js
+++ implementations/sorting/challenges/merge-k-sorted-lists.js
@@ -11,51 +11,99 @@
  */
 
 class ListNode {
-	constructor(val, next) {
-		this.val = val === undefined ? 0 : val;
-		this.next = next === undefined ? null : next;
+	constructor(val = 0, next = null) {
+		this.val = val;
+		this.next = next;
 	}
 }
 
-const mergeKLists = (lists) => {
-	lists = lists.filter((list) => list !== null);
-	if (lists.length === 0) return null;
-	if (lists.length === 1) return lists[0];
+// const mergeKLists = (lists) => {
+// 	lists = lists.filter((list) => list !== null);
+// 	if (lists.length === 0) return null;
+// 	if (lists.length === 1) return lists[0];
 
-	let head = null;
-	let curr = null;
-	let len = lists.length;
+// 	let head = null;
+// 	let curr = null;
+// 	let len = lists.length;
 
-	while (len) {
-		let minIdx = 0;
-		for (let i = 0; i < len; i++) {
-			const list = lists[i];
-			if (lists[minIdx] === null) minIdx = i;
-			else if (list.val < lists[minIdx].val) minIdx = i;
-		}
-		if (head !== null) {
-			curr.next = new ListNode(lists[minIdx].val);
+// 	while (len) {
+// 		let minIdx = 0;
+// 		for (let i = 0; i < len; i++) {
+// 			const list = lists[i];
+// 			if (lists[minIdx] === null) minIdx = i;
+// 			else if (list.val < lists[minIdx].val) minIdx = i;
+// 		}
+// 		if (head !== null) {
+// 			curr.next = new ListNode(lists[minIdx].val);
+// 			curr = curr.next;
+// 		} else {
+// 			head = new ListNode(lists[minIdx].val);
+// 			curr = head;
+// 		}
+
+// 		lists[minIdx] = lists[minIdx].next;
+// 		if (lists[minIdx] === null) {
+// 			lists[minIdx] = lists[len - 1];
+// 			len--;
+// 		}
+// 	}
+
+// 	return head;
+// };
+
+// tests
+
+const merge = (list1, list2) => {
+	if (list1 === null) return list2;
+	if (list2 === null) return list1;
+
+	let head;
+	let curr;
+	let min = list1.val;
+	if (list2.val < list1.val) {
+		min = list2.val;
+		list2 = list2.next;
+	} else list1 = list1.next;
+
+	head = new ListNode(min);
+	curr = head;
+
+	while (list1 !== null && list2 !== null) {
+		if (list2.val < list1.val) {
+			curr.next = new ListNode(list2.val);
 			curr = curr.next;
+			list2 = list2.next;
 		} else {
-			head = new ListNode(lists[minIdx].val);
-			curr = head;
+			curr.next = new ListNode(list1.val);
+			curr = curr.next;
+			list1 = list1.next;
 		}
-
-		lists[minIdx] = lists[minIdx].next;
-		if (lists[minIdx] === null) {
-			lists[minIdx] = lists[len - 1];
-			len--;
-		}
+	}
+	while (list1 !== null) {
+		curr.next = new ListNode(list1.val);
+		curr = curr.next;
+		list1 = list1.next;
+	}
+	while (list2 !== null) {
+		curr.next = new ListNode(list2.val);
+		curr = curr.next;
+		list2 = list2.next;
 	}
 
 	return head;
 };
 
-// tests
+const mergeKLists = (lists) => {
+	if (lists.length === 0) return null;
+	if (lists.length === 1) return lists[0];
 
-const list1 = new ListNode(1, new ListNode(4, new ListNode(5)));
-const list2 = new ListNode(1, new ListNode(3, new ListNode(4)));
-const list3 = new ListNode(2, new ListNode(6));
+	let head = lists[0];
+	for (let i = 1; i < lists.length; i++) {
+		head = merge(head, lists[i]);
+	}
+
+	return head;
+};
 
 const printList = (head) => {
 	let curr = head;
@@ -67,4 +115,10 @@ const printList = (head) => {
 	return str;
 };
 
-console.log(printList(mergeKLists([list1, list2, list3])));
+const list1 = new ListNode(1, new ListNode(4, new ListNode(5, new ListNode(8, new ListNode(9, new ListNode(10))))));
+const list2 = new ListNode(1, new ListNode(3, new ListNode(4)));
+const list3 = new ListNode(2, new ListNode(6, new ListNode(7)));
+
+const result = mergeKLists([list1, list2, list3]);
+
+console.log(printList(result) === '1,1,2,3,4,4,5,6,7,8,9,10,' ? true : printList(result));
--- implementations/sorting/sorting.js
+++ implementations/sorting/sorting.js
@@ -10,7 +10,16 @@ const insertionSort = (arr) => {
 	}
 };
 
-const mergeSort = (arr) => {};
+const mergeSort = (arr, start = 0, end = arr.length - 1) => {
+	if (end - start + 1 <= 1) return arr;
+	const mid = Math.floor((start + end) / 2);
+	if (start < end) {
+		mergeSort(arr, start, mid);
+		mergeSort(arr, mid + 1, end);
+		merge(arr, start, mid, end);
+	}
+	return arr;
+};
 
 const quickSort = (arr) => {};
 
@@ -20,20 +29,34 @@ const bucketSort = (arr) => {};
 
 const swap = (arr, i, j) => ([arr[i], arr[j]] = [arr[j], arr[i]]);
 
+const merge = (arr, start, mid, end) => {
+	const left = arr.slice(start, mid + 1);
+	const right = arr.slice(mid + 1, end + 1);
+	let i = 0,
+		j = 0,
+		k = start;
+	while (i < left.length && j < right.length) {
+		if (left[i] <= right[j]) arr[k++] = left[i++];
+		else arr[k++] = right[j++];
+	}
+	while (i < left.length) arr[k++] = left[i++];
+	while (j < right.length) arr[k++] = right[j++];
+};
+
 // ! Main function
 
 const sort = (arr) => {
-	insertionSort(arr);
-	return arr;
+	// insertionSort(arr);
+	return mergeSort(arr);
 };
 
 // ! Test function
 
-const test = () => { 
-  const arr1 = [5, 2, 3, 1];
-  const arr2 = [5, 1, 1, 2, 0, 0];
-  console.log(sort(arr1).toString() === [1, 2, 3, 5].toString());
-  console.log(sort(arr2).toString() === [0, 0, 1, 1, 2, 5].toString());
+const test = () => {
+	const arr1 = [5, 2, 3, 1];
+	const arr2 = [5, 1, 1, 2, 0, 0];
+	console.log(sort(arr1).toString() === [1, 2, 3, 5].toString());
+	console.log(sort(arr2).toString() === [0, 0, 1, 1, 2, 5].toString());
 };
 
 test();
